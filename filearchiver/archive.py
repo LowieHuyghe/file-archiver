aea9866352e375719f36aad0ba0205933cce8ef5
Lowie Huyghe <iam@lowiehuyghe.com>
2018-03-14 15:58:10 +0100

    Improved exporting of google drive files.

--- filearchiver/archive.py
+++ filearchiver/archive.py
@@ -5,13 +5,13 @@ from filearchiver.archiver.descriptor import Descriptor
 import os
 from scriptcore.filesystem.path import Path
 from scriptcore.console.option import Option
-
+import json
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client import tools
 from oauth2client.file import Storage
 from apiclient.discovery import build
 import httplib2
-from scriptcore.filesystem.mimetype import MimeType
+from scriptcode.encoding.encoding import Encoding
 
 
 class Archive(CuiScript):
@@ -34,14 +34,63 @@ class Archive(CuiScript):
         self._descriptor = Descriptor()
 
         self._google_drive_service = None
-        self._google_drive_supported_extensions = ['.gdoc', '.gsheet', '.gslides', '.gdraw']
-        self._google_drive_unsupported_extensions = ['.gform', '.gmap', '.gsite']
-        self._google_drive_associated_extensions = {
-            '.gdoc': ['.pdf', '.docx'],
-            '.gsheet': ['.pdf', '.xslx'],
-            '.gslides': ['.pdf', '.pptx'],
-            '.gdraw': ['.pdf']
+        self._google_drive_supported_extensions = {
+            '.gdoc': 'application/vnd.google-apps.document',
+            '.gsheet': 'application/vnd.google-apps.spreadsheet',
+            '.gslides': 'application/vnd.google-apps.presentation',
+            '.gdraw': 'application/vnd.google-apps.drawing',
+            '.gscript': 'application/vnd.google-apps.script'
         }
+        self._google_drive_supported_mime_types = {
+            'application/vnd.google-apps.document': [
+                'application/pdf',
+                'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'application/vnd.oasis.opendocument.text',
+                # 'application/rtf',  # Gives internal error on Google Drive API
+                'application/zip',
+                'text/plain'
+            ],
+            'application/vnd.google-apps.spreadsheet': [
+                'application/pdf',
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'application/x-vnd.oasis.opendocument.spreadsheet',
+                'application/zip',
+                'text/csv'
+            ],
+            'application/vnd.google-apps.presentation': [
+                'application/pdf',
+                'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                'application/vnd.oasis.opendocument.presentation',
+                'text/plain'
+            ],
+            'application/vnd.google-apps.drawing': [
+                'application/pdf',
+                'image/jpeg',
+                'image/png',
+                'image/svg+xml'
+            ],
+            'application/vnd.google-apps.script': [
+                'application/vnd.google-apps.script+json'
+            ]
+        }
+        self._google_drive_associated_extensions = {
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document': '.docx',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': '.xlsx',
+            'application/vnd.openxmlformats-officedocument.presentationml.presentation': '.pptx',
+            'application/vnd.oasis.opendocument.text': '.odt',
+            'application/x-vnd.oasis.opendocument.spreadsheet': '.ods',
+            'application/vnd.oasis.opendocument.presentation': '.odp',
+            'application/pdf': '.pdf',
+            'image/jpeg': '.jpg',
+            'image/png': '.png',
+            'image/svg+xml': '.svg',
+            'application/rtf': '.rtf',
+            'text/plain': '.txt',
+            'text/csv': '.csv',
+            'application/zip': '.zip',
+            'application/vnd.google-apps.script+json': '.json'
+        }
+        self._google_drive_unsupported_extensions = ['.gform', '.gmap', '.gsite']
 
     def _run(self):
         """
@@ -89,84 +138,48 @@ class Archive(CuiScript):
         :return:    void
         """
 
-        unsupported_extension_continue = False
-
-        for dir_path, dir_names, files in os.walk(directory):
-            dir_name = os.path.basename(dir_path)
-            for file in files:
-                file_name, extension = os.path.splitext(file)
+        for dir_path, dirs, files in os.walk(directory):
+            for file_name in files:
+                file_path = os.path.join(dir_path, file_name)
+                file_name_without_extension, extension = os.path.splitext(file_name)
 
                 # Unsupported extension
                 if extension in self._google_drive_unsupported_extensions:
-                    if not unsupported_extension_continue:
-                        description = 'Can\'t backup "%s" from Google Drive as exporting is not supported.\n' \
-                                      '  Would you like to continue with "%s"?' % (os.path.join(dir_path, file), directory)
-                        if self.input.yes_no(description):
-                            unsupported_extension_continue = True
-                        else:
-                            return False
+                    description = 'Can\'t backup "%s" from Google Drive as exporting is not supported.\n' \
+                                  '  Would you like to continue with "%s"?' % (file_path, directory)
+                    if not self.input.yes_no(description):
+                        return False
 
                 # Supported extension
-                if extension in self._google_drive_supported_extensions:
+                elif extension in self._google_drive_supported_extensions:
+                    # Get Doc id
+                    with open(file_path, 'rb') as open_file:
+                        google_drive_file_content = json.load(open_file)
+
+                        if 'doc_id' not in google_drive_file_content:
+                            description = 'Can\'t backup "%s" from Google Drive as file-id could not be found.\n' \
+                                          '  Would you like to continue with "%s"?' % (file_path, directory)
+                            if not self.input.yes_no(description):
+                                return False
+
+                        google_drive_file_id = Encoding.normalize(google_drive_file_content['doc_id'])
+
                     # Drive service
                     drive_service = self._get_google_drive_service()
                     if drive_service is None:
                         description = 'Could not initiate Google Drive service to backup Google Docs.\n' \
                                       '  For more info: see README.md.\n' \
                                       '  Would you like to continue with "%s"?' % directory
-                        return self.input.yes_no(description)
-
-                    # Setup query
-                    mime_type, encoding = MimeType.guess_type(extension)
-                    query = 'name = "%s" and mimeType = "%s"' % (file_name, mime_type)
-
-                    # Fetch result
-                    response = drive_service.files().list(q=query).execute()
-                    response_files = response.get('files', [])
-
-                    # No results
-                    if not len(response_files):
-                        description = 'Couldn\'t backup "%s" from Google Drive.\n' \
-                                      '  Would you like to continue with "%d"?' % (os.path.join(dir_path, file), directory)
                         if not self.input.yes_no(description):
                             return False
-                        continue
-
-                    # More than one match found, filter
-                    response_files_info = dict()
-                    if len(response_files) > 1:
-                        def filter_response_files(response_file):
-                            response = drive_service.files().get(fileId=response_file.get('id'), fields='parents, createdTime, modifiedTime').execute()
-                            response_files_info[response_file.get('id')] = response
-                            if not len(response.get('parents')):
-                                return True
-                            response = drive_service.files().get(fileId=response.get('parents')[0], fields='name').execute()
-                            return dir_name == response.get('name')
-
-                        response_files = filter(filter_response_files, response_files)
-
-                    # Still more than one match
-                    if len(response_files) > 1:
-                        description = 'There are multiple files found that match "%s".\n' \
-                                      '  Which one is the correct one?' % os.path.join(dir_path, file)
-                        pick_options = []
-                        for response_file in response_files:
-                            response_file_info = response_files_info[response_file.get('id')]
-                            created_time = '%s %s' % (response_file_info.get('createdTime')[0:10], response_file_info.get('createdTime')[11:19])
-                            modified_time = '%s %s' % (response_file_info.get('modifiedTime')[0:10], response_file_info.get('modifiedTime')[11:19])
-                            pick_options.append('%s (created:%s, modified:%s)' % (response_file.get('name'), created_time, modified_time))
-                        picked_index = None
-                        while picked_index is None:
-                            picked_index = self.input.pick(pick_options, description)
-
-                    response_file = response_files[0]
 
                     # Backup for each associated extension
-                    for associated_extension in self._google_drive_associated_extensions[extension]:
-                        associated_mime_type, associated_encoding = MimeType.guess_type(associated_extension)
+                    mime_type = self._google_drive_supported_extensions[extension]
+                    for associated_mime_type in self._google_drive_supported_mime_types[mime_type]:
+                        associated_extension = self._google_drive_associated_extensions[associated_mime_type]
 
                         # Fetch response
-                        response = drive_service.files().export(fileId=response_file.get('id'), mimeType=associated_mime_type).execute()
+                        response = drive_service.files().export(fileId=google_drive_file_id, mimeType=associated_mime_type).execute()
                         # Save to disk
                         backup_file_name = os.path.join(dir_path, '%s.bak%s' % (file_name, associated_extension))
                         with open(backup_file_name, 'w') as backup_file:
